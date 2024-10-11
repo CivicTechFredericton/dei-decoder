32b89e808ee71fbe1d0c53fc1cd7e2ab28f133e6
ashwantmanikoth <ashwanta75@gmail.com>
2024-10-11 15:25:40 -0300

    feat(app): Footer Logo changed
    Civic tech logo for the footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <footer className="w-full bg-white text-black py-6 border-t-2 border-gray-100">
-      <p className="text-center">&copy; 2024 DEI Decoder. All rights reserved.</p>
+    <footer className="w-full flex bg-white text-black py-6 border-t-2 border-gray-100 justify-center">
+      <Image
+        src="/icons/civic_tech_logo.png"
+        alt="Document Icon"
+        width={100} // Replace with your icon's width
+        height={100} // Replace with your icon's height
+        className=""
+      />{" "}
     </footer>
   );
 };
