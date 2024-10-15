cd63b070c3faaab680ce9aa8ca089c8fc4340012
Subhan Haq <shaq@unb.ca>
2024-10-15 17:00:07 -0300

    adding mesh gradiant to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 import Link from "next/link";
 import Layout from "./components/Layout";
 import { ArrowRightIcon } from "@heroicons/react/24/outline"; // v2 path for outline icons
+import "./globals.css";
 
 const Home = () => {
   return (
     <Layout>
       {/* Using min-h-screen to ensure the content fills the height, and padding to account for footer space */}
-      <div className="min-h-screen flex flex-col justify-center p-10 md:p-60 bg-gradient-to-r bg-white">
+      <div className="min-h-screen flex flex-col justify-center p-10 md:pt-60 bg-gradient-to-r bg-white"> 
         <main className="flex flex-col text-center items-center justify-center">
-          <h2 className="mx-8 mb-8 text-4xl font-bold text-main-color w-[1000px] h-auto leading-[54px]">
+          <div className="gradient z-0"></div>
+          <h2 className="mx-8 mb-8 text-4xl font-bold text-main-color w-[1000px] h-auto leading-[54px] z-10">
             Ensure your communications reflect your commitment to diversity,
             equity, and inclusion by analyzing bias in your job postings
             communications and more.
           </h2>
-          <p className="text-xl text-sub-color w-[1000px] h-auto leading-[26px]">
+          <p className="text-xl text-sub-color w-[1000px] h-auto leading-[26px] z-10">
             In seconds the DEI Decoder will enable your organization to create
             more inclusive content by identifying gaps and providing you
             actionable recommendations.
           </p>
-          <Link href="/decoder">
+          <Link href="/decoder" className="z-10">
             <button className="px-2 mt-8 py-3 w-[235px] h-[52px] bg-main-color text-white rounded-full flex items-center justify-between">
               <p className="text-xl font-bold text-center flex-grow">
                 Try it now
