df405f4e39bfc3bd08ea9ed45407fb1237d357b6
Subhan Haq <93729876+SubyDoo@users.noreply.github.com>
2024-10-15 19:03:56 -0300

    UI/mesh gradient (#4)
    * adding mesh gradiant to homepage
    
    * adding theme colors for about, try, and faq

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,16 +4,16 @@ const Navbar = () => {
     <nav className="flex justify-evenly items-center text-nowrap">
       <h1 className="text-4xl font-bold pr-12">
         <a href="/">
-          <span className="text-[#A567D8]">D</span>
-          <span className="text-[#00B7FF]">E</span>
-          <span className="text-[#E71980]">I </span>
+          <span className="text-about-color">D</span>
+          <span className="text-try-color">E</span>
+          <span className="text-faq-color">I </span>
           <span className="text-main-color">Decoder</span>
         </a>
       </h1>
       <ul className="flex space-x-12 text-xl font-extrabold">
-        <li><a href="/about" className="text-[#A567D8]">About</a></li>
-        <li><a href="/decoder" className="text-[#00B7FF]">Try it Now</a></li>
-        <li><a href="/faq" className="text-[#E71980]">FAQ</a></li>
+        <li><a href="/about" className="text-about-color">About</a></li>
+        <li><a href="/decoder" className="text-try-color">Try it Now</a></li>
+        <li><a href="/faq" className="text-faq-color">FAQ</a></li>
       </ul>
     </nav>
   </header>
