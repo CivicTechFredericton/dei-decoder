426fd1ac09a0ea077bc3100584cb93e4251e935e
Long Nguyen <90327629+longnguyen1305@users.noreply.github.com>
2025-03-27 13:16:31 -0300

    add comments

--- src/app/api/reportfeedback/route.ts
+++ src/app/api/reportfeedback/route.ts
@@ -1,9 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 const nodemailer = require("nodemailer")
 
+/*
+    NodeMailer documentation: https://nodemailer.com/
+    Create a transporter object with Gmail service
+    Email credentials pulled from env
+*/
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
+        // Email address and password of sender
         user: process.env.MAIL_SENDER,
         pass: process.env.MAIL_PASSWORD
     },
@@ -13,11 +19,14 @@ const transporter = nodemailer.createTransport({
     },
 });
 
+// Handle POST requests to send feedback emails
 export async function POST(req: NextRequest) {
 
     try {
-
+        // Parse incoming request body (feedback as JSON)
         const feedback = await req.json();
+
+        // Send an email to a receipient using the transporter
         const info = await transporter.sendMail({
             from: `DEI Decoder <${process.env.MAIL_SENDER}>`,
             to: `Admin <${process.env.MAIL_RECIPIENT}>`,
